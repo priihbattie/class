a3d6d5a1281f2c8d69f807f5b12c4651e8fce5a8
priihbattie <135707297+priihbattie@users.noreply.github.com>
2024-08-14 11:54:54 -0300

    Add files via upload

---
+++ class.js
@@ -0,0 +1,681 @@
+class Produto:
+    
+    d
+
+ 
+def __init__(self, id_produto, nome, categoria, preco, quantidade_estoque, data_fabricacao, data_validade=None, fornecedor=None):
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.fornecedor = fornecedor
+
+    
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.fornecedor = fornecedor
+
+ 
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.fornecedor = fornece
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.fornecedor = 
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.fornec
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        self.f
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+        s
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+    
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_validade
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = data_va
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoqueclass Produto:
+    
+        d
+    
+     
+    def __init__(self, id_produto, nome, categoria, preco, quantidade_estoque, data_fabricacao, data_validade=None, fornecedor=None):
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.fornecedor = fornecedor
+    
+        
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.fornecedor = fornecedor
+    
+     
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.fornecedor = fornece
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.fornecedor = 
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.fornec
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            self.f
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+            s
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+        
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_validade
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = data_va
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_validade = d
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.data_valid
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            self.dat
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+            se
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricacao
+        
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fabricaca
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = data_fab
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricacao = da
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fabricaca
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.data_fab
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            self.da
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+            
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_estoque
+     
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantidade_est
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = quantida
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoque = qu
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidade_estoq
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.quantidad
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            self.qua
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+            se
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+         
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = preco
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.preco = 
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            self.pr
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+            se
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categoria
+        
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = categori
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categoria = ca
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.categori
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            self.ca
+    
+            self.id_produto = id_produto
+            self.nome = nome
+            s
+    
+            self.id_produto = id_produto
+            self.nome = nome
+       
+    
+            self.id_produto = id_produto
+            self.nome = no
+    
+            self.id_produto = id_produto
+            self.nom
+    
+            self.id_produto = id_produto
+            s
+    
+            self.id_produto = id_produto
+      
+    
+            self.id_produto = id_prod
+    
+            self.id_produto = i
+    
+            self.id_produ
+    
+            self.
+            s
+    
+      
+    def __repr__(self):
+            
+            r
+    
+        
+    return (f"Produto(ID: {self.id_produto}, Nome: {self.nome}, Categoria: {self.categoria}, "
+                    
+                    
+    
+                
+    
+            
+    
+        
+    f"Preço: {self.preco}, Quantidade em Estoque: {self.quantidade_estoque}, "
+                    
+                   
+    
+              
+    
+         
+    f"Data de Fabricação: {self.data_fabricacao}, Data de Validade: {self.data_validade}, "
+                    
+                   
+    
+             
+    
+       
+    f"Fornecedor: {self.fornecedor})")
+        self.data_fabricacao = data_fabricacao
+        self.data_validade = d
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.data_valid
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        self.dat
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+        se
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricacao
+    
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fabricaca
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = data_fab
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricacao = da
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fabricaca
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.data_fab
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        self.da
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+        
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_estoque
+ 
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantidade_est
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = quantida
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoque = qu
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidade_estoq
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.quantidad
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        self.qua
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+        se
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+     
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = preco
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.preco = 
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        self.pr
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+        se
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categoria
+    
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = categori
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categoria = ca
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.categori
+
+        self.id_produto = id_produto
+        self.nome = nome
+        self.ca
+
+        self.id_produto = id_produto
+        self.nome = nome
+        s
+
+        self.id_produto = id_produto
+        self.nome = nome
+   
+
+        self.id_produto = id_produto
+        self.nome = no
+
+        self.id_produto = id_produto
+        self.nom
+
+        self.id_produto = id_produto
+        s
+
+        self.id_produto = id_produto
+  
+
+        self.id_produto = id_prod
+
+        self.id_produto = i
+
+        self.id_produ
+
+        self.
+        s
+
+  
+def __repr__(self):
+        
+        r
+
+    
+return (f"Produto(ID: {self.id_produto}, Nome: {self.nome}, Categoria: {self.categoria}, "
+                
+                
+
+            
+
+        
+
+    
+f"Preço: {self.preco}, Quantidade em Estoque: {self.quantidade_estoque}, "
+                
+               
+
+          
+
+     
+f"Data de Fabricação: {self.data_fabricacao}, Data de Validade: {self.data_validade}, "
+                
+               
+
+         
+
+   
+f"Fornecedor: {self.fornecedor})")
---
+++ constructor.js
@@ -0,0 +1,143 @@
+class Pessoa:
+    
+    de
+
+ 
+def __init__(self, nome, data_nascimento, cpf, endereco):
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = endereco
+
+    
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = endereco
+
+    de
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = endereco
+
+   
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = endereco
+
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = enderec
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = ende
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.endereco = 
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.enderec
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self.end
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+        self
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+       
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = cpf
+  
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.cpf = 
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+        self.
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+       
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+   
+
+        self.nome = nome
+        self.data_nascimento = data_nascimento
+
+        self.nome = nome
+        self.data_nascimento = data_nasci
+
+        self.nome = nome
+        self.data_nascimento = data_n
+
+        self.nome = nome
+        self.data_nascimento = d
+
+        self.nome = nome
+        self.data_nasciment
+
+        self.nome = nome
+        self.data_nasc
+
+        self.nome = nome
+        self.dat
+
+        self.nome = nome
+        se
+
+        self.nome = nome
+    
+
+        self.nome = nom
+
+        self.nome
+
+        self
+
+       
+
+  
+def __repr__(self):
+        
+        ret
+
+      
+
+   
+return (f"Pessoa(Nome: {self.nome}, Data de Nascimento: {self.data_nascimento}, "
+                
+                f
+
+              
+
+           
+
+        
+
+    
+f"CPF: {self.cpf}, Endereço: {self.endereco})")
